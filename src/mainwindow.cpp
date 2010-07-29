c966b784518ecbdc76e813b2858fc965bf5db9c1
Piotr Luczko <piotr.luczko@gmail.com>
2010-07-29 22:00:22 +0000

    fix ignore, friends layout

--- src/mainwindow.cpp
+++ src/mainwindow.cpp
@@ -72,7 +72,7 @@ MainWindow::MainWindow(QWidget *parent) : QMainWindow(parent)
 
 // settings
     settings.clear();
-    settings.setValue("version", "1.0.6.343");
+    settings.setValue("version", "1.0.6.344");
     settings.setValue("debug", "off");
     settings.setValue("logged", "off");
     settings.setValue("busy", "off");
