3ed8fd3fd07142fb2e17d4fa85abb93420900d48
Piotr Luczko <piotr.luczko@gmail.com>
2010-08-02 16:05:24 +0000

    fix license

--- src/mainwindow.cpp
+++ src/mainwindow.cpp
@@ -76,7 +76,7 @@ MainWindow::MainWindow(QWidget *parent) : QMainWindow(parent)
 
 // settings
     settings.clear();
-    settings.setValue("version", "1.0.6.355");
+    settings.setValue("version", "1.0.6.356");
     settings.setValue("debug", "off");
     settings.setValue("logged", "off");
     settings.setValue("busy", "off");
