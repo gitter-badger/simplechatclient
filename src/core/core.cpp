80499a5de842c33238f56df03169912c7bbd279f
Piotr Luczko <piotr.luczko@gmail.com>
2011-08-08 20:24:38 +0000

    fixed cmake

--- src/core/core.cpp
+++ src/core/core.cpp
@@ -109,7 +109,7 @@ void Core::createGui()
 void Core::createSettings()
 {
     // default settings
-    settings["version"] = "1.1.2.969";
+    settings["version"] = "1.1.2.970";
     settings["logged"] = "off";
     settings["busy"] = "off";
     settings["away"] = "off";
