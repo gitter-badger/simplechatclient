fc97d82d28c410969c1664a0b1343c105ab2b8e5
Piotr Luczko <piotr.luczko@gmail.com>
2011-09-20 18:53:15 +0000

    fixed clearsh

--- clear.sh
+++ clear.sh
@@ -1,13 +1,13 @@
 #! /bin/bash
-rm -rdf CMakeFiles CMakeCache.txt cmake_install.cmake cmake_uninstall.cmake install_manifest.txt Makefile scc.pro.user scc-config.h
-cd translations && rm -rdf CMakeFiles cmake_install.cmake Makefile
-cd ..
-cd src && rm -rdf CMakeFiles scc.dir cmake_install.cmake Makefile
-cd ..
 if [ "$(lsb_release -i | awk '{print $3}')" = "Ubuntu" ]; then
 	sudo make uninstall
 else
 	su -c 'make uninstall'
 fi
 make clean
+rm -rdf CMakeFiles CMakeCache.txt cmake_install.cmake cmake_uninstall.cmake install_manifest.txt Makefile scc.pro.user scc-config.h
+cd translations && rm -rdf CMakeFiles cmake_install.cmake Makefile
+cd ..
+cd src && rm -rdf CMakeFiles scc.dir cmake_install.cmake Makefile
+cd ..
 clear
