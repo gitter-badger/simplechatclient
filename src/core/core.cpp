a0cad109fcb837b1b7eafe2a9bfb49279b7ffe96
Piotr Luczko <piotr.luczko@gmail.com>
2012-03-10 14:30:09 +0000

    fixed changelog

--- src/core/core.cpp
+++ src/core/core.cpp
@@ -132,7 +132,7 @@ void Core::createSettings()
     settings["debug"] = strDebug;
 
     // default settings
-    settings["version"] = "1.4.0.1203";
+    settings["version"] = "1.4.0.1204";
     settings["available_version"] = "";
     settings["whats_new"] = "";
     settings["motd"] = "";
