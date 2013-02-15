04a7964e39ba268c4e8b9fe1febe2051379eebe9
mero <piotr.luczko@gmail.com>
2013-02-16 00:28:19 +0100

    fixed compile on linux

--- build/build-linux/compile.sh
+++ build/build-linux/compile.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
-cp /usr/bin/scc ./pkg-debian/usr/bin/
-cp -r /usr/share/scc/*.* ./pkg-debian/usr/share/scc/
+cp /usr/bin/scc ./pkg-debian/usr/bin/scc
+cp -r /usr/share/scc/* ./pkg-debian/usr/share/scc/
 
 cd pkg-debian
 find . -type f ! -regex '.*\.hg.*' ! -regex '.*?debian-binary.*' ! -regex '.*?DEBIAN.*' -printf '%P ' | xargs md5sum > DEBIAN/md5sums 
 cd ..
 
-fakeroot dpkg -b pkg-debian/ scc-1.7.0.0.deb 
+fakeroot dpkg -b pkg-debian/ scc-1.7.0.0.deb
+fakeroot alien -c -k -r scc-1.7.0.0.deb
