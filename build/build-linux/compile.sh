0397b9c47ccb901c093bf79666db3b8d2b141ceb
mero <piotr.luczko@gmail.com>
2013-02-16 00:58:51 +0100

    fixed compile on linux

--- build/build-linux/compile.sh
+++ build/build-linux/compile.sh
@@ -2,6 +2,10 @@
 cp /usr/bin/scc ./pkg-debian/usr/bin/scc
 cp -r /usr/share/scc/* ./pkg-debian/usr/share/scc/
 
+chmod 755 ./pkg-debian/usr/bin/scc
+find ./pkg-debian/usr/share/scc/ -type d -exec chmod 755 {} \;
+find ./pkg-debian/usr/share/scc/ -type f -exec chmod 644 {} \;
+
 cd pkg-debian
 find . -type f ! -regex '.*\.hg.*' ! -regex '.*?debian-binary.*' ! -regex '.*?DEBIAN.*' -printf '%P ' | xargs md5sum > DEBIAN/md5sums 
 cd ..
