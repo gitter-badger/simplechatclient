c476126f95ef94197a1130e7b911dc6149254014
dmarkowicz <dmarkowicz@alari.pl>
2014-02-09 21:12:22 +0100

    QToolButton restyled

--- src/core/main.cpp
+++ src/core/main.cpp
@@ -115,7 +115,14 @@ int main(int argc, char *argv[])
 #ifdef Q_WS_WIN
     // workaround for QTBUG-21808
     app.setStyleSheet("QTabBar QToolButton {"
-                      "background-color: qlineargradient(x1:0, y1:0, x2:0, y2:1, stop:0 #f6f7fa, stop:1 #dadbde);"
+                      "background-color: qlineargradient(x1:0, y1:0, x2:0, y2:1, stop:0 #f2f2f2, stop:0.5 #ebebeb, stop:0.51 #dddddd, stop:1 #cfcfcf);"
+                      "margin: 1px;"
+                      "border: 1px solid #707070;"
+                      "border-radius: 2px;"
+                      "}"
+                      "QTabBar QToolButton:pressed {"
+                      "margin: 2px 0 0 2px;"
+                      "border: 1px solid #000000;"
                       "}");
 #endif
 
