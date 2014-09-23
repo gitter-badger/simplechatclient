c1f3a062e81a9cdd086afbe037f0685493b5d729
mero <piotr.luczko@gmail.com>
2014-09-23 20:01:21 +0200

    qt 5.3.2 in compile; added env settings

--- README.rst
+++ README.rst
@@ -25,6 +25,10 @@ Windows
 `Microsoft Visual C++ 2008 SP1 Redistributable Package (x86)
 <http://www.microsoft.com/download/en/details.aspx?displaylang=en&id=5582>`_
 
+Example Environment variables for development:
+PATH c:\Qt\5.3\mingw482_32\bin;c:\Qt\Tools\mingw482_32\bin;C:\Program Files (x86)\CMake\bin;
+QT_DIR c:\Qt\5.3\mingw482_32\bin\
+
 Linux Debian/Ubuntu packages
 =========
 ::
--- compile.bat
+++ compile.bat
@@ -2,10 +2,10 @@
 
 set SCC_DIR=%CD%
 set DESTINATION="C:\scc\release"
-set QT_DIR="c:\Qt\Qt5.3.1\5.3\mingw482_32"
+set QT_DIR="c:\Qt\5.3\mingw482_32"
 set QCA2_DIR="C:\qca-2.0.3"
 set OPENSSL_DIR="C:\OpenSSL-Win32"
-set MINGW_DIR="c:\Qt\Qt5.3.1\Tools\mingw482_32"
+set MINGW_DIR="c:\Qt\Tools\mingw482_32"
 set CMAKE_DIR="C:\Program Files\CMake"
 set CMAKE32_DIR="C:\Program Files (x86)\CMake"
 
@@ -23,7 +23,7 @@ exit
 )
 
 if not exist %QT_DIR%\bin\qmake.exe (
-echo Download Qt 5.3.0 for MinGW from http://qt-project.org/downloads
+echo Download Qt 5.3.0 for MinGW from http://www.qt.io/download/
 pause
 exit
 )
